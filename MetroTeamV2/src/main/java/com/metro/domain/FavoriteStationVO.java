d1c641571397c5269e44ea96c333446cf0ef4cb5
hhjeong30 <hhjeong30@gmail.com>
2019-07-25 18:27:45 +0900

    즐겨찾기vo

---
+++ MetroTeamV2/src/main/java/com/metro/domain/FavoriteStationVO.java
@@ -0,0 +1,56 @@
+package com.metro.domain;
+
+public class FavoriteStationVO {
+
+	private String sid;
+	private String sname;
+	private String direction;
+	private String lno;
+	private String info;
+	private String addr;
+	private String stel;
+	
+	public String getSid() {
+		return sid;
+	}
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getDirection() {
+		return direction;
+	}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+	public String getLno() {
+		return lno;
+	}
+	public void setLno(String lno) {
+		this.lno = lno;
+	}
+	public String getInfo() {
+		return info;
+	}
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getStel() {
+		return stel;
+	}
+	public void setStel(String stel) {
+		this.stel = stel;
+	}
+}
+
